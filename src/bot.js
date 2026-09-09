d2aa7a571d1d3d192bf100cb9a352d50f6ef9b59
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2026-09-09 13:13:15 +0400

    create server for deployment

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,7 @@ const { config, assertReadyForBot } = require("./config");
 const logger = require("./logger");
 const { runPipeline } = require("./pipeline");
 const { formatProductResult } = require("./format/telegramFormatter");
+const http = require("http");
 
 assertReadyForBot();
 
@@ -109,3 +110,14 @@ bot.launch().then(() => logger.info("Bot started"));
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+const PORT = process.env.PORT || 3000;
+
+const healthServer = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK");
+});
+
+healthServer.listen(PORT, "0.0.0.0", () => {
+  logger.info(`HTTP health server listening on port ${PORT}`);
+});
